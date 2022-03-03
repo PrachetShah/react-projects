7eccfaa40c10d26a6571ff9650e41fcecf87a24c
PrachetShah <prachetshah25@gmail.com>
2022-03-03 10:24:49 +0530

    project 5 menu created with dynamic approach

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button className="filter-btn" onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        );
+      })}
+      {/* <button className="filter-btn" onClick={() => filterItems("all")}>
+        All
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
+        BreakFast
+      </button> */}
+    </div>
+  );
 };
 
 export default Categories;
