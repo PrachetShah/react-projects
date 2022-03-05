f5abe4b7f1d80265e40d7f5ea3c5e5f657bba77d
PrachetShah <prachetshah25@gmail.com>
2022-03-05 11:04:05 +0530

    project 8 lorem ipsum generator completed

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,53 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello World");
+    //In this case count as string will work but if you want it as int you do this in this way
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of some boring lorem ipsum?</h3>
+      <form
+        onSubmit={handleSubmit}
+        className="lorem-form"
+        style={{ marginBottom: "10px" }}
+      >
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <label className="lorem-form" style={{ margin: "2rem" }}>
+        Maximum of 8 paragraphs can be generated
+      </label>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
